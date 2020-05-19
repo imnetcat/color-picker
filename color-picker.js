e7fde7bf2fc3223b6b3dc40a03b9347b8ac6b41f
imnetcat <guskov.danil@gmail.com>
2020-05-19 23:23:41 +0300

    разделил на html\css\js файлы

---
+++ color-picker.js
@@ -0,0 +1,15 @@
+﻿cp = ColorPicker(document.getElementById('pcr'), document.getElementById('picker'), 
+function(hex, hsv, rgb, mousePicker, mousepcr) {
+currentColor = hex;
+     ColorPicker.positionIndicators(
+     document.getElementById('pcr-indicator'),
+     document.getElementById('picker-indicator'),
+     mousepcr, mousePicker);
+                    
+    document.getElementById('hex').innerHTML = hex;
+    document.getElementById('rgb').innerHTML = 'rgb(' + rgb.r.toFixed() + ',' + rgb.g.toFixed() + ',' + rgb.b.toFixed() + ')';
+    document.getElementById('hsv').innerHTML = 'hsv(' + hsv.h.toFixed() + ',' + hsv.s.toFixed(2) + ',' + hsv.v.toFixed(2) + ')';
+					
+	document.getElementById('pcr_bg').style.backgroundColor = hex;
+});
+    cp.setHex('#D4EDFB');
