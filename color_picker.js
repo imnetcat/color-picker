4221fceb4463ea1ee4e51b55355df7b612130f19
imnetcat <guskov.danil@gmail.com>
2020-05-20 00:01:17 +0300

    обернул всё в класс, обновил до es6

--- color_picker.js
+++ color_picker.js
@@ -1,18 +1,25 @@
-﻿
+﻿'use strict';
+
+class ColorPalete {
+    static Init() {
+        const cp = ColorPicker(document.getElementById('pcr'), document.getElementById('picker'),
+            function (hex, hsv, rgb, mousePicker, mousepcr) {
+                const currentColor = hex;
+                ColorPicker.positionIndicators(
+                    document.getElementById('pcr-indicator'),
+                    document.getElementById('picker-indicator'),
+                    mousepcr, mousePicker);
+
+                document.getElementById('hex').innerHTML = hex;
+                document.getElementById('rgb').innerHTML = 'rgb(' + rgb.r.toFixed() + ',' + rgb.g.toFixed() + ',' + rgb.b.toFixed() + ')';
+                document.getElementById('hsv').innerHTML = 'hsv(' + hsv.h.toFixed() + ',' + hsv.s.toFixed(2) + ',' + hsv.v.toFixed(2) + ')';
+
+                document.getElementById('pcr_bg').style.backgroundColor = hex;
+            });
+        cp.setHex('#D4EDFB');
+    }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-    cp = ColorPicker(document.getElementById('pcr'), document.getElementById('picker'),
-        function (hex, hsv, rgb, mousePicker, mousepcr) {
-            currentColor = hex;
-            ColorPicker.positionIndicators(
-                document.getElementById('pcr-indicator'),
-                document.getElementById('picker-indicator'),
-                mousepcr, mousePicker);
-
-            document.getElementById('hex').innerHTML = hex;
-            document.getElementById('rgb').innerHTML = 'rgb(' + rgb.r.toFixed() + ',' + rgb.g.toFixed() + ',' + rgb.b.toFixed() + ')';
-            document.getElementById('hsv').innerHTML = 'hsv(' + hsv.h.toFixed() + ',' + hsv.s.toFixed(2) + ',' + hsv.v.toFixed(2) + ')';
-
-            document.getElementById('pcr_bg').style.backgroundColor = hex;
-        });
-    cp.setHex('#D4EDFB');
+    ColorPalete.Init();
 });
